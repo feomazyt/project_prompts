28049861d3183a350be206fd0d81ea89e43f5ca2
Krzysiek <72891@student.pb.edu.pl>
2024-10-18 10:23:34 +0200

    suspense fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Promptopia",
@@ -18,7 +19,7 @@ const RootLayout = ({ children }) => {
 
           <main className="app">
             <Nav />
-            {children}
+            <Suspense>{children}</Suspense>
           </main>
         </Provider>
       </body>
